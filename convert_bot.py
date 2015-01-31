e1a7c9363c8cf37f76cd6a17f16c8c746e0eb2e0
Dominic <dominicperotti@gmail.com>
2015-01-31 19:05:37 +0000

    second line test

--- convert_bot.py
+++ convert_bot.py
@@ -5,3 +5,5 @@ from pprint import pprint
 r = praw.Reddit('Currency conversion bot by /u/dperotti v 1.0.' 'URL: https://github.com/dominicperotti/reddit_bot/tree/master')
 
 r.login()
+already_done = []
+keyWords = ['CurrencyConversion:']
