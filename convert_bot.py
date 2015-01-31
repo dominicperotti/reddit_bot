93a5e20bf372a9931d0d7fc5215fbbd66f255b4d
Dominic <dominicperotti@gmail.com>
2015-01-31 18:57:48 +0000

    Readme

--- convert_bot.py
+++ convert_bot.py
@@ -2,3 +2,6 @@ import time
 import praw
 from pprint import pprint
 
+r = praw.Reddit('Currency conversion bot by /u/dperotti v 1.0.' 'URL: https://github.com/dominicperotti/reddit_bot/tree/master')
+
+r.login()
