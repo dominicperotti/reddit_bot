86336c1cf2e689bad5a55097eeabc6d62ba0a4a9
Dominic <dominicperotti@gmail.com>
2015-02-01 04:37:30 +0000

    last commit hopefully

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # converto_bot
-repo for CPSC reddit bot
-
-Reddit bot that converts USD amounts to other currencies
+-Reddit bot that converts USD amounts to other currencies
+-Called by the prompt "currencyconvert: " (upper or lowercase) followed by a USD amount declared with a "$"
+-Currently not backwards compatable
--- convert_bot.py
+++ convert_bot.py
@@ -19,14 +19,17 @@ CAD = Decimal(1.26605)
 BTC = Decimal(0.00430)
 JPY = Decimal(117.80)
 AUD = Decimal(1.28660)
+MXN = Decimal(14.975945)
 try:
 	from bot_configs import *
-
+	readfile = open('done_file.txt', 'r')
+	already_done = [line.strip() for line in readfile]
+	readfile.close()
 except ImportError:
 	pass
 	
 r  = praw.Reddit(USER_AGENT)
-#r.login("BOT_USERNAME", "BOT_PASSWORD")
+r.login(BOT_USERNAME, BOT_PASSWORD)
 subreddit = r.get_subreddit(SUBREDDIT)
 
 
@@ -41,27 +44,38 @@ def convert():
 				#Get dollar amount and convert
 				reply_text = ""
 				comment_array = comment_text.split()
-				#print "Found one! comment_array is: "
-				#print comment_array
+				#for each word in the comment, check to see if it begins with a '$' if it does, print out the conversions for it
 				for word in comment_array:
 					if "$" in word and word.index("$") == 0:
-						amount = word.replace("$", "")
-						float_amount = Decimal(amount)
-						eur_amount = str((float_amount * EUR).quantize(Decimal('.01'))).replace(".", ",")
-						gbp_amount = str((float_amount * GBP).quantize(Decimal('.01')))
-						cad_amount = str((float_amount * CAD).quantize(Decimal('.01')))
-						btc_amount = str((float_amount * BTC).quantize(Decimal('.00001')))
-						jpy_amount = str((float_amount * JPY).quantize(Decimal('1')))
-						aud_amount = str((float_amount * AUD).quantize(Decimal('.01')))
-						reply_text =  word + " conversions: \n\nAUD: " + aud_amount + "\n\nBTC: " + btc_amount + "\n\nCAD: " + cad_amount + "\n\nEUR: " + eur_amount + "\n\nGBP: " + gbp_amount + "\n\nJPY: " + jpy_amount	
-						#comment.reply(reply_text)
-						print "Reply text is : " + reply_text
+						try:
+							amount = word.replace("$", "")
+							float_amount = Decimal(amount)
+							eur_amount = str((float_amount * EUR).quantize(Decimal('.01'))).replace(".", ",")
+							gbp_amount = str((float_amount * GBP).quantize(Decimal('.01')))
+							cad_amount = str((float_amount * CAD).quantize(Decimal('.01')))
+							btc_amount = str((float_amount * BTC).quantize(Decimal('0.00000001')))
+							jpy_amount = str((float_amount * JPY).quantize(Decimal('1')))
+							aud_amount = str((float_amount * AUD).quantize(Decimal('.01')))
+							mxn_amount = str((float_amount * MXN).quantize(Decimal('.01')))
+							reply_text =(word + " conversions: \n\nAUD: " + aud_amount 
+							+ "\n\nBTC: " + btc_amount + "\n\nCAD: " + cad_amount + "\n\nEUR: " +
+							eur_amount + "\n\nGBP: " + gbp_amount + "\n\nJPY: " + jpy_amount +
+							"\n\nMXN: "+ mxn_amount)	
+							comment.reply(reply_text)
+							#print "Reply text is : " + reply_text
+						except Exception as error_message1:
+							print error_message1
+		writefile = open('done_file.txt', 'w')
+		already_done2 = map(lambda x:x+'\n', already_done)
+		writefile.writelines(already_done2)
+		writefile.close()
 		print "Done!"
-	except Exception as error_message:
-		print error_message
-
+	except Exception as error_message2:
+		print error_message2
 
 print "searching through comments"
-while True:
-	convert()
-	time.sleep(1800)
+convert()
+#repeat every 30mins (currently set up so that it never leaves the while loop. switching to crontabs
+#while True:
+#	convert()
+#	time.sleep(1800)
